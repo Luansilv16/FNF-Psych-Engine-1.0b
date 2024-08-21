43605e9ca436bbb4bfb8b17f1977549f3dc605a0
Luansilv16 <164948628+Luansilv16@users.noreply.github.com>
2024-08-20 23:45:27 -0300

    Initial commit

---
+++ source/Main.hx
@@ -0,0 +1,201 @@
+package;
+
+import debug.FPSCounter;
+import backend.Highscore;
+import flixel.FlxGame;
+import haxe.io.Path;
+import openfl.Lib;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.display.StageScaleMode;
+import lime.app.Application;
+import states.TitleState;
+import mobile.backend.MobileScaleMode;
+import openfl.events.KeyboardEvent;
+import lime.system.System as LimeSystem;
+import mobile.objects.MobileControls;
+#if mobile
+import mobile.states.CopyState;
+#end
+#if linux
+import lime.graphics.Image;
+
+@:cppInclude('./external/gamemode_client.h')
+@:cppFileCode('
+	#define GAMEMODE_AUTO
+')
+#end
+
+class Main extends Sprite
+{
+	var game = {
+		width: 1280, // WINDOW width
+		height: 720, // WINDOW height
+		initialState: TitleState, // initial game state
+		zoom: -1.0, // game state bounds
+		framerate: 60, // default framerate
+		skipSplash: true, // if the default flixel splash screen should be skipped
+		startFullscreen: false // if the game should start at fullscreen mode
+	};
+
+	public static var fpsVar:FPSCounter;
+
+	public static final platform:String = #if mobile "Phones" #else "PCs" #end;
+
+	// You can pretty much ignore everything from here on - your code should go in your states.
+
+	public static function main():Void
+	{
+		Lib.current.addChild(new Main());
+		#if cpp
+		cpp.NativeGc.enable(true);
+		#elseif hl
+		hl.Gc.enable(true);
+		#end
+	}
+
+	public function new()
+	{
+		super();
+		#if mobile
+		#if android
+		StorageUtil.requestPermissions();
+		#end
+		Sys.setCwd(StorageUtil.getStorageDirectory());
+		#end
+		backend.CrashHandler.init();
+
+		#if windows
+		@:functionCode("
+			#include <windows.h>
+			#include <winuser.h>
+			setProcessDPIAware() // allows for more crisp visuals
+			DisableProcessWindowsGhosting() // lets you move the window and such if it's not responding
+		")
+		#end
+
+		if (stage != null)
+		{
+			init();
+		}
+		else
+		{
+			addEventListener(Event.ADDED_TO_STAGE, init);
+		}
+		#if VIDEOS_ALLOWED
+		hxvlc.util.Handle.init(#if (hxvlc >= "1.8.0")  ['--no-lua'] #end);
+		#end
+	}
+
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+		{
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+		}
+
+		setupGame();
+	}
+
+	private function setupGame():Void
+	{
+		#if (openfl <= "9.2.0")
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (game.zoom == -1.0)
+		{
+			var ratioX:Float = stageWidth / game.width;
+			var ratioY:Float = stageHeight / game.height;
+			game.zoom = Math.min(ratioX, ratioY);
+			game.width = Math.ceil(stageWidth / game.zoom);
+			game.height = Math.ceil(stageHeight / game.zoom);
+		}
+		#else
+		if (game.zoom == -1.0)
+			game.zoom = 1.0;
+		#end
+
+		#if LUA_ALLOWED
+		Mods.pushGlobalMods();
+		#end
+		Mods.loadTopMod();
+
+		FlxG.save.bind('funkin', CoolUtil.getSavePath());
+
+		Highscore.load();
+
+		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
+		Controls.instance = new Controls();
+		ClientPrefs.loadDefaultKeys();
+		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
+		addChild(new FlxGame(game.width, game.height, #if (mobile && MODS_ALLOWED) CopyState.checkExistingFiles() ? game.initialState : CopyState #else game.initialState #end, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+
+		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
+		addChild(fpsVar);
+		Lib.current.stage.align = "tl";
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+		if(fpsVar != null) {
+			fpsVar.visible = ClientPrefs.data.showFPS;
+		}
+
+		#if linux
+		var icon = Image.fromFile("icon.png");
+		Lib.current.stage.window.setIcon(icon);
+		#end
+
+		#if html5
+		FlxG.autoPause = false;
+		FlxG.mouse.visible = false;
+		#end
+
+		FlxG.fixedTimestep = false;
+		FlxG.game.focusLostFramerate = #if mobile 30 #else 60 #end;
+		#if web
+		FlxG.keys.preventDefaultKeys.push(TAB);
+		#else
+		FlxG.keys.preventDefaultKeys = [TAB];
+		#end
+
+		#if DISCORD_ALLOWED
+		DiscordClient.prepare();
+		#end
+		MobileControls.initSave();
+
+		#if desktop FlxG.stage.addEventListener(KeyboardEvent.KEY_UP, toggleFullScreen); #end
+
+		#if android FlxG.android.preventDefaultKeys = [BACK]; #end
+
+		#if mobile
+		LimeSystem.allowScreenTimeout = ClientPrefs.data.screensaver; 		
+		FlxG.scaleMode = new MobileScaleMode();
+		#end
+
+		// shader coords fix
+		FlxG.signals.gameResized.add(function (w, h) {
+			if(fpsVar != null)
+				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
+		     if (FlxG.cameras != null) {
+			   for (cam in FlxG.cameras.list) {
+				if (cam != null && cam.filters != null)
+					resetSpriteCache(cam.flashSprite);
+			   }
+			}
+
+			if (FlxG.game != null)
+			resetSpriteCache(FlxG.game);
+		});
+	}
+
+	static function resetSpriteCache(sprite:Sprite):Void {
+		@:privateAccess {
+		        sprite.__cacheBitmap = null;
+			sprite.__cacheBitmapData = null;
+		}
+	}
+
+	function toggleFullScreen(event:KeyboardEvent) {
+		if(Controls.instance.justReleased('fullscreen'))
+			FlxG.fullscreen = !FlxG.fullscreen;
+	}
+}
---
+++ source/options/OptionsState.hx
@@ -0,0 +1,182 @@
+package options;
+
+import states.MainMenuState;
+import backend.StageData;
+import mobile.substates.MobileControlSelectSubState;
+#if (target.threaded)
+import sys.thread.Thread;
+import sys.thread.Mutex;
+#end
+
+class OptionsState extends MusicBeatState
+{
+	var options:Array<String> = [
+		'Note Colors',
+		'Controls',
+		'Adjust Delay and Combo',
+		'Graphics',
+		'Visuals',
+		'Gameplay'
+		#if TRANSLATIONS_ALLOWED , 'Language' #end
+		,'Mobile Options'
+	];
+	private var grpOptions:FlxTypedGroup<Alphabet>;
+	private static var curSelected:Int = 0;
+	public static var menuBG:FlxSprite;
+	public static var onPlayState:Bool = false;
+	#if (target.threaded) var mutex:Mutex = new Mutex(); #end
+
+	function openSelectedSubstate(label:String) {
+		if (label != "Adjust Delay and Combo"){
+			removeTouchPad();
+			persistentUpdate = false;
+		}
+		switch(label)
+		{
+			case 'Note Colors':
+				openSubState(new options.NotesSubState());
+			case 'Controls':
+				switch (controls.mobileC)
+				{
+					case true:
+						persistentUpdate = false;
+						openSubState(new MobileControlSelectSubState());
+					default: openSubState(new options.ControlsSubState());
+				}
+			case 'Graphics':
+				openSubState(new options.GraphicsSettingsSubState());
+			case 'Visuals':
+				openSubState(new options.VisualsSettingsSubState());
+			case 'Gameplay':
+				openSubState(new options.GameplaySettingsSubState());
+			case 'Adjust Delay and Combo':
+				MusicBeatState.switchState(new options.NoteOffsetState());
+			case 'Mobile Options':
+				openSubState(new mobile.options.MobileOptionsSubState());
+			#if TRANSLATIONS_ALLOWED
+			case 'Language':
+				openSubState(new options.LanguageSubState());
+			#end
+		}
+	}
+
+	var selectorLeft:Alphabet;
+	var selectorRight:Alphabet;
+
+	override function create()
+	{
+		#if DISCORD_ALLOWED
+		DiscordClient.changePresence("Options Menu", null);
+		#end
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.antialiasing = ClientPrefs.data.antialiasing;
+		bg.color = 0xFFea71fd;
+		bg.updateHitbox();
+
+		bg.screenCenter();
+		add(bg);
+
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		add(grpOptions);
+
+		for (num => option in options)
+		{
+			var optionText:Alphabet = new Alphabet(0, 0, Language.getPhrase('options_$option', option), true);
+			optionText.screenCenter();
+			optionText.y += (92 * (num - (options.length / 2))) + 45;
+			grpOptions.add(optionText);
+		}
+
+		selectorLeft = new Alphabet(0, 0, '>', true);
+		add(selectorLeft);
+		selectorRight = new Alphabet(0, 0, '<', true);
+		add(selectorRight);
+
+		changeSelection();
+		ClientPrefs.saveSettings();
+
+		addTouchPad('UP_DOWN', 'A_B');
+
+		#if (target.threaded)
+		Thread.create(()->{
+			mutex.acquire();
+
+			for (music in VisualsSettingsSubState.pauseMusics)
+			{
+				if (music.toLowerCase() != "none")
+					Paths.music(Paths.formatToSongPath(music));
+			}
+
+			mutex.release();
+		});
+		#end
+
+		super.create();
+	}
+
+	override function closeSubState()
+	{
+		super.closeSubState();
+		ClientPrefs.saveSettings();
+		#if DISCORD_ALLOWED
+		DiscordClient.changePresence("Options Menu", null);
+		#end
+		controls.isInSubstate = false;
+        removeTouchPad();
+		addTouchPad('UP_DOWN', 'A_B');
+		persistentUpdate = true;
+	}
+
+	var exiting = false;
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if(!exiting) {
+			if (controls.UI_UP_P)
+				changeSelection(-1);
+			if (controls.UI_DOWN_P)
+				changeSelection(1);
+
+			if (controls.BACK)
+			{
+				exiting = true;
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				if(onPlayState)
+				{
+					StageData.loadDirectory(PlayState.SONG);
+					LoadingState.loadAndSwitchState(new PlayState());
+					FlxG.sound.music.volume = 0;
+				}
+				else MusicBeatState.switchState(new MainMenuState());
+			}
+			else if (controls.ACCEPT) openSelectedSubstate(options[curSelected]);
+		}
+	}
+	
+	function changeSelection(change:Int = 0)
+	{
+		curSelected = FlxMath.wrap(curSelected + change, 0, options.length - 1);
+
+		for (num => item in grpOptions.members)
+		{
+			item.targetY = num - curSelected;
+			item.alpha = 0.6;
+			if (item.targetY == 0)
+			{
+				item.alpha = 1;
+				selectorLeft.x = item.x - 63;
+				selectorLeft.y = item.y;
+				selectorRight.x = item.x + item.width + 15;
+				selectorRight.y = item.y;
+			}
+		}
+		FlxG.sound.play(Paths.sound('scrollMenu'));
+	}
+
+	override function destroy()
+	{
+		ClientPrefs.loadPrefs();
+		super.destroy();
+	}
+}
